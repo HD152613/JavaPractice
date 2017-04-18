39bbfeb2096528047ccdce3a84beace12109871b
suboum <whaterbum99@naver.com>
2017-04-18 10:39:59 +0900

    123

---
+++ JavaPractice/src/org/dimigo/oop/FamilyMember.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *	 |_FamilyMember
+ *
+ *
+ * 1 개요 	:
+ * 2. 작성일 :  2017. 4. 18.
+ * </pre>
+ * 
+ * @auther :  	suboum
+ * @Versiion : 	1.0
+ *
+ */
+public class FamilyMember {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/oop/PiggyBank.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *	 |_PiggyBank
+ *
+ *
+ * 1 개요 	:
+ * 2. 작성일 :  2017. 4. 18.
+ * </pre>
+ * 
+ * @auther :  	suboum
+ * @Versiion : 	1.0
+ *
+ */
+public class PiggyBank {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
