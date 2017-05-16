7e04b7c8a6a6ff1c60e7f4ad1dd8d1dc6704f053
suboum <whaterbum99@naver.com>
2017-05-16 10:29:49 +0900

    123

---
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *	 |_Galaxy
+ *
+ *
+ * 1 개요 	:
+ * 2. 작성일 :  2017. 5. 16.
+ * </pre>
+ * 
+ * @auther :  	suboum
+ * @Versiion : 	1.0
+ *
+ */
+public class Galaxy {
+
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *	 |_SmartPhoneTest
+ *
+ *
+ * 1 개요 	:
+ * 2. 작성일 :  2017. 5. 16.
+ * </pre>
+ * 
+ * @auther :  	suboum
+ * @Versiion : 	1.0
+ *
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
